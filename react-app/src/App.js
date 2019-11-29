f1aea507f980a84b61a42b4d7e7d31043e6ca5f5
Suzan Nakatudde <suzannakatudde@Suzans-MacBook-Pro.local>
2019-11-29 05:33:23 -0600

    serve react app with aflask-server

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>My Token = {window.token}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
